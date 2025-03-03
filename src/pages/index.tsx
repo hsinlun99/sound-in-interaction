36d98e28e60ac5232c44f79482853963c6c14e94
hsinlun99 <hsinlun.chiu@gmail.com>
2025-03-03 23:56:22 +0100

    feat: :lipstick: add map image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,16 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import AudioPlayer from "@/components/AudioPlayer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-        <AudioPlayer />
-        
-      </main>
+    <div>
+      <Image
+        className="w-full object-cover"
+        src={"/image/map.svg"}
+        alt="sweden map"
+        width={0}
+        height={0}
+        sizes="100vw"
+      />
     </div>
   );
 }
