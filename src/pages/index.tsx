35b28f274cedbcfe613033e969b58db9ce02fe7d
hsinlun99 <hsinlun.chiu@gmail.com>
2025-03-04 02:09:24 +0100

    feat: :sparkles: a start button to create audioContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,23 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import AnimalButton from "@/components/AnimalButton";
+import StartButton from "@/components/StartButton";
 
 export default function Home() {
 
   const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
+  const handleStart = () => {
+    if (!audioContext) {
       const context = new AudioContext();
       setAudioContext(context);
-    }
-  }, []);
 
-  if (!audioContext) {
-    return <div>Loading...</div>;
-  }
+      context.resume().then(() => {
+        console.log("AudioContext is resumed and ready");
+      });
+
+    }
+  };
 
   return (
     <div className="h-screen flex items-center justify-center overflow-hidden">
@@ -29,15 +31,26 @@ export default function Home() {
           sizes="100vh"
           priority
         />
-        <div className="absolute top-[20%] left-[40%]">
-          <AnimalButton imagePath="/image/animal-eagle.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
-        </div>
-        <div className="absolute top-[50%] left-[45%]">
-          <AnimalButton imagePath="/image/animal-goose.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
-        </div>
-        <div className="absolute top-[60%] left-[10%]">
-          <AnimalButton imagePath="/image/animal-wolverine.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
-        </div>
+
+        {!audioContext && (
+          <div className="absolute top-[50%] left-[50%]">
+            <StartButton onClick={handleStart} />
+          </div>
+        )}
+
+        {audioContext && (
+          <>
+            <div className="absolute top-[20%] left-[40%]">
+              <AnimalButton imagePath="/image/animal-eagle.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
+            </div>
+            <div className="absolute top-[50%] left-[45%]">
+              <AnimalButton imagePath="/image/animal-goose.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
+            </div>
+            <div className="absolute top-[60%] left-[10%]">
+              <AnimalButton imagePath="/image/animal-wolverine.svg" audioPath="/audio/outfoxing.mp3" audioContext={audioContext} />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
